cf9e711e1dc3e188d59596ce2efbd87fb94f0a63
Alena Midgen <alena.midgen@gmail.com>
2021-01-30 00:55:04 -0500

    enums of patient done

--- standbylist/api/models.py
+++ standbylist/api/models.py
@@ -2,19 +2,66 @@ from django.db import models
 
 # Create your models here.
 class Patient(models.Model):
-    age = models.PositiveIntegerField();
-    firstName = models.CharField();
-    lastName = models.CharField();
-    phoneNumber = models.CharField(max_length=10);
-    email = models.CharField();
+    NODOSE='0D'
+    ONEDOSE='1D'
+    TWODOSE='2D'
+    VACCINATION_STATUS=(
+        (NODOSE, 'No doses'),
+        (ONEDOSE, 'One dose'),
+        (TWODOSE, 'Two doses'),
+    )
+
+    UNNOTIFIED='Unnotified'
+    NOTIFIED='Notified'
+    CONFIRMED='Confirmed'
+    DECLINED='Declined'
+    TIMEOUT='Timed out'
+    VACCINATED='Vaccinated'
+    NOTIFICATION_STATUS=(
+        (UNNOTIFIED, 'Unnotified'),
+        (NOTIFIED, 'Notified'),
+        (CONFIRMED, 'Confirmed'),
+        (DECLINED, 'Declined'),
+        (TIMEOUT, 'Timed out'),
+        (VACCINATED, 'Vaccinated'),
+    )
+
+    TIERONE='Tier 1'
+    TIERTWO='Tier 2'
+    TIERTHREE='Tier 3'
+    TIERFOUR='Tier 4'
+    OCCUPATION=(
+        (TIERONE, 'Tier 1'), 
+        (TIERTWO, 'Tier 2'),
+        (TIERTHREE, 'Tier 3'),
+        (TIERFOUR, 'Tier 4'),
+    )
+
+    CAR='Car'
+    PUBLICTRANSIT='Public transit'
+    WALK='Walk'
+    BIKE='Bike'
+    OTHER='Other'
+    MODE_OF_TRANSIT=(
+        (CAR, 'Car'),
+        (PUBLICTRANSIT, 'Public transit'),
+        (WALK, 'Walk'),
+        (BIKE, 'Bike'),
+        (OTHER, 'Other'),
+    )
+
+    age = models.PositiveIntegerField()
+    firstName = models.CharField()
+    lastName = models.CharField()
+    phoneNumber = models.CharField(max_length=10)
+    email = models.CharField()
     #how do we do home address???
     #homeAddress
-    #how do we do enum?
-    #enum vaccination status
-    #enum notification status
-    #enum occupation
-    #enum mode of transport to clinic
-    highRiskHousehold = models.BooleanField();
-    healthcareNum = models.CharField(unique=True);
+    vaccinationStatus = models.CharField(max_length=2, choices=VACCINATION_STATUS, default=NODOSE)
+    notificationStatus = models.CharField(choices=NOTIFICATION_STATUS, default=UNNOTIFIED)
+    occupation = models.CharField(choices=OCCUPATION)
+    transport = models.CharField(choices=MODE_OF_TRANSIT)
+    highRiskHousehold = models.BooleanField()
+    healthcareNum = models.CharField(unique=True)
     #can an attribute be an object we create? 
-    #Clinic home clinic
+    #enum Clinic home clinic
