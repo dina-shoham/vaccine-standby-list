f5d592f0ab99dc08ddd8222c1b19015af910e68c
dina-shoham <dina.shoham@gmail.com>
2021-01-29 20:58:41 -0700

    THE APP RUNS WOOOO

--- standbylist/standbylist/settings.py
+++ standbylist/standbylist/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'standbylist.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
