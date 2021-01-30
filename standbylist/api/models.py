1cddb2d7e18c86b3097cc548986f1a9005c56626
Alena Midgen <alena.midgen@gmail.com>
2021-01-30 00:27:39 -0500

    starting models

--- standbylist/api/models.py
+++ standbylist/api/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
 # Create your models here.
-#class Patient(models.Model):
+class Patient(models.Model):
+    age = models.PositiveIntegerField();
+    firstName = models.CharField();
+    lastName = models.CharField();
+    phoneNumber = models.CharField(max_length=10);
+    email = models.CharField();
+    #how do we do home address???
+    #homeAddress
+    #how do we do enum?
+    #enum vaccination status
+    #enum notification status
+    #enum occupation
+    #enum mode of transport to clinic
+    highRiskHousehold = models.BooleanField();
+    healthcareNum = models.CharField(unique=True);
+    #can an attribute be an object we create? 
+    #Clinic home clinic
