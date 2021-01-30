9b01058e25008e32d0a831055376b772d23d96b5
dina-shoham <dina.shoham@gmail.com>
2021-01-29 23:25:35 -0700

    frontend url routing

---
+++ standbylist/frontend/src/components/ClinicLogin.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ClinicLogin extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>clinic login</p>;
+  }
+}
---
+++ standbylist/frontend/src/components/PatientForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PatientForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>patient form</p>;
+  }
+}
